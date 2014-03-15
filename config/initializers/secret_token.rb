97bc7f3e7bb48b5138876d62fcb09fea022c302e
John LeBoeuf-Little <worldnamer@worldnamer.com>
2014-03-15 00:09:35 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lucid::Application.config.secret_token = '0ef9f2d907c079a479210d8eed8b0c87f03d1c56818359eaa5d5777c7fa36bb82a9b69a9b9784117db2ba94fe29a1501051500be1e3bda066d5ef6cb5b5043d1'
